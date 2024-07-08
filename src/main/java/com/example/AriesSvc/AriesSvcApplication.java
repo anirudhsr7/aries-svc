24fe11766005b4536f3ef1501e0c7c07d1f1decf
anirudhsr7 <anirudhsr140@gmail.com>
2024-07-08 21:22:03 +0530

    first commit

---
+++ src/main/java/com/example/AriesSvc/AriesSvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.AriesSvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriesSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AriesSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AriesSvc/AriesSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AriesSvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriesSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
